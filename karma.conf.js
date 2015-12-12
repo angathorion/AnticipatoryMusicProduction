e7552c77d961bac2c8c23f84d8f5b95bb7bbc40b
angathorion <tanghuansong@gmail.com>
2015-12-12 18:09:35 +0800

    Added karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,83 @@
+// Karma configuration
+// Generated on Sat Dec 12 2015 11:23:22 GMT+0800 (Malay Peninsula Standard Time)
+
+module.exports = function (config) {
+    var configuration = {
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['qunit'],
+
+
+        // list of files / patterns to load in the browser
+        files: [
+            'src/js/vendor/*.js',
+            'src/js/anticipatory_music_producer.js',
+            'src/js/painter.js',
+            'src/js/interface.js',
+            'src/js/scheduler.js',
+            'src/js/bridge.js',
+            'qunit/tests.js'
+        ],
+
+
+        // list of files to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base : 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun      : true,
+
+        // Concurrency level
+        // how many browser should be started simultanous
+        concurrency: Infinity
+    };
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
+};
