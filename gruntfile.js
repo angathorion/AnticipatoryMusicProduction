ac9bc204faa8a5694bd7ec90052e893444083083
angathorion <tanghuansong@gmail.com>
2015-12-12 19:06:26 +0800

    Use grunt-karma

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,6 @@
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
-        qunit: {
-            //files: ['qunit/index.html']
-            all: {
-                options: {
-                    urls: [
-                        'http://localhost:3000/qunit/index.html',
-                    ],
-                    timeout: 10000
-                }
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 3000,
-                    base: '.'
-                }
-            }
-        },
         karma: {
             unit: {
                 configFile: 'karma.conf.js'
